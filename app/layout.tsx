dc07956ad8ceb0fc34d3dbaa269850eeb5c344e2
outoftheboxdev1 <130626635+outoftheboxdev1@users.noreply.github.com>
2025-10-26 15:22:55 -0400

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "stocksVisor",
+  description: "StocksVisor is your all-in-one stock analytics app with real-time prices, interactive charts, AI-powered insights, personalized alerts, and daily market news — empowering smarter investing decisions.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
